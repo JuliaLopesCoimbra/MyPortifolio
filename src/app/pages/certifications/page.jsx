ec3b33ef4b1110f87482770e99c438d9b99f92eb
unknown <juliacristinalopes2607@gmail.com>
2025-03-07 11:22:34 -0300

    aaa

--- src/app/pages/certifications/page.jsx
+++ src/app/pages/certifications/page.jsx
@@ -2,45 +2,50 @@
 
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation"; // Para navegação no Next.js
-import Header from "../Header";
 
 const certifications = [
   {
     title: "",
     image: "/certifications/programming/reactProject.jpg",
     type: "Java",
-    description: "No contexto de um curso intensivo de desenvolvimento full stack, tive a oportunidade de realizar um projeto: a criação de um sistema integrado para uma pizzaria. O objetivo era otimizar o processo de pedidos e entrega, melhorando a comunicação entre os garçons e a equipe de cozinha. Utilizei React Native para desenvolver o aplicativo utilizado pelos garçons. Este aplicativo permitia que eles registrassem os pedidos dos clientes de forma rápida e eficiente, enviando-os diretamente para um dashboard operado pela cozinha, desenvolvido em React. O dashboard exibia os pedidos em tempo real, permitindo que a equipe de cozinha gerenciasse de maneira eficaz as demandas e informasse quando os pratos estavam prontos para serem servidos. No backend, optei por Node.js para gerenciar a lógica de negócios e a comunicação entre o frontend e o banco de dados. Esta escolha foi crucial para garantir uma transmissão de dados rápida e segura. Para o gerenciamento dos dados, implementei um banco de dados usando Postbird, acessado através da plataforma Beekeeper. Essa configuração foi essencial para manter a organização dos dados de pedidos e a eficiência operacional do sistema.",
+    description:
+      "No contexto de um curso intensivo de desenvolvimento full stack, tive a oportunidade de realizar um projeto: a criação de um sistema integrado para uma pizzaria. O objetivo era otimizar o processo de pedidos e entrega, melhorando a comunicação entre os garçons e a equipe de cozinha. Utilizei React Native para desenvolver o aplicativo utilizado pelos garçons. Este aplicativo permitia que eles registrassem os pedidos dos clientes de forma rápida e eficiente, enviando-os diretamente para um dashboard operado pela cozinha, desenvolvido em React. O dashboard exibia os pedidos em tempo real, permitindo que a equipe de cozinha gerenciasse de maneira eficaz as demandas e informasse quando os pratos estavam prontos para serem servidos. No backend, optei por Node.js para gerenciar a lógica de negócios e a comunicação entre o frontend e o banco de dados. Esta escolha foi crucial para garantir uma transmissão de dados rápida e segura. Para o gerenciamento dos dados, implementei um banco de dados usando Postbird, acessado através da plataforma Beekeeper. Essa configuração foi essencial para manter a organização dos dados de pedidos e a eficiência operacional do sistema.",
   },
   {
     title: "",
     image: "/certifications/programming/oracleData.jpg",
     type: "React",
-    description: "Ao longo do segundo semestre de 2024, mergulhei profundamente nos conceitos e práticas essenciais de SQL e PL/SQL, explorando as capacidades avançadas do Oracle Database. O curso abordou desde a criação e gestão de bancos de dados até técnicas sofisticadas de manipulação e recuperação de dados, preparando-me para enfrentar problemas reais de banco de dados com eficiência e confiança.",
+    description:
+      "Ao longo do segundo semestre de 2024, mergulhei profundamente nos conceitos e práticas essenciais de SQL e PL/SQL, explorando as capacidades avançadas do Oracle Database. O curso abordou desde a criação e gestão de bancos de dados até técnicas sofisticadas de manipulação e recuperação de dados, preparando-me para enfrentar problemas reais de banco de dados com eficiência e confiança.",
   },
 
   {
     title: "",
     image: "/certifications/programming/maratonaProgramacao.jpg",
     type: "Java",
-    description: "A maratona consistiu em uma série de problemas de programação que variavam em dificuldade, abrangendo algoritmos, estruturas de dados, matemática e lógica. Cada desafio tinha como objetivo testar nossa capacidade de pensar de forma crítica e desenvolver soluções eficientes em um curto período de tempo.",
+    description:
+      "A maratona consistiu em uma série de problemas de programação que variavam em dificuldade, abrangendo algoritmos, estruturas de dados, matemática e lógica. Cada desafio tinha como objetivo testar nossa capacidade de pensar de forma crítica e desenvolver soluções eficientes em um curto período de tempo.",
   },
   {
     title: "",
     image: "/certifications/programming/java.jpg",
     type: "React",
-    description: "Durante o curso, explorei extensivamente os fundamentos de JAVA, incluindo sintaxe, controle de fluxo, orientação a objetos, e manipulação de exceções. A metodologia de ensino da Rocketseat, focada em prática e aplicação real, permitiu que eu não apenas aprendesse a teoria, mas também aplicasse o conhecimento em projetos práticos e desafios de codificação.",
+    description:
+      "Durante o curso, explorei extensivamente os fundamentos de JAVA, incluindo sintaxe, controle de fluxo, orientação a objetos, e manipulação de exceções. A metodologia de ensino da Rocketseat, focada em prática e aplicação real, permitiu que eu não apenas aprendesse a teoria, mas também aplicasse o conhecimento em projetos práticos e desafios de codificação.",
   },
   {
     title: "",
     image: "/certifications/programming/hackathon2024.jpg",
     type: "Estrutura de Dados",
-    description: "Tive o privilégio de participar do Hackathon de 2024 organizado pelo Centro Universitário de Franca - UNIFACEF, um evento que reuniu mentes criativas para solucionar desafios contemporâneos usando a tecnologia. O tema do nosso projeto foi particularmente inovador e desafiador: como auxiliar o usuário a medir sua felicidade e orientá-lo sobre práticas que ajudem na sensação de felicidade.Nosso objetivo era criar uma ferramenta capaz de coletar dados relevantes sobre o bem-estar e a felicidade das pessoas, incorporando variáveis socioeconômicas que pudessem influenciar esses indicadores. O questionário foi projetado para ser intuitivo e acessível, facilitando a participação de uma ampla demografia.Utilizamos tecnologias avançadas para garantir a análise precisa dos dados coletados, empregando algoritmos de inteligência artificial para interpretar as respostas e gerar insights significativos. A plataforma foi desenvolvida com a intenção de ser adquirida e utilizada por instituições de saúde, que poderiam empregar essas informações para fomentar crescimento científico e desenvolver políticas mais eficazes voltadas à saúde mental e bem-estar da população.Trabalhar em equipe sob a pressão de um hackathon foi uma experiência intensamente gratificante e educativa. A colaboração entre os membros da equipe foi fundamental para moldar nossa solução final, combinando conhecimento técnico com perspectivas criativas para abordar o desafio de forma holística.",
+    description:
+      "Tive o privilégio de participar do Hackathon de 2024 organizado pelo Centro Universitário de Franca - UNIFACEF, um evento que reuniu mentes criativas para solucionar desafios contemporâneos usando a tecnologia. O tema do nosso projeto foi particularmente inovador e desafiador: como auxiliar o usuário a medir sua felicidade e orientá-lo sobre práticas que ajudem na sensação de felicidade.Nosso objetivo era criar uma ferramenta capaz de coletar dados relevantes sobre o bem-estar e a felicidade das pessoas, incorporando variáveis socioeconômicas que pudessem influenciar esses indicadores. O questionário foi projetado para ser intuitivo e acessível, facilitando a participação de uma ampla demografia.Utilizamos tecnologias avançadas para garantir a análise precisa dos dados coletados, empregando algoritmos de inteligência artificial para interpretar as respostas e gerar insights significativos. A plataforma foi desenvolvida com a intenção de ser adquirida e utilizada por instituições de saúde, que poderiam empregar essas informações para fomentar crescimento científico e desenvolver políticas mais eficazes voltadas à saúde mental e bem-estar da população.Trabalhar em equipe sob a pressão de um hackathon foi uma experiência intensamente gratificante e educativa. A colaboração entre os membros da equipe foi fundamental para moldar nossa solução final, combinando conhecimento técnico com perspectivas criativas para abordar o desafio de forma holística.",
   },
   {
     title: "",
     image: "/certifications/programming/eventoLattes.jpg",
     type: "Estrutura de Dados",
-    description: "O workshop focou em estratégias eficazes para estruturar e destacar informações no Currículo Lattes, uma ferramenta indispensável para acadêmicos, cientistas e estudantes no Brasil. Aprendi a organizar minhas qualificações, projetos de pesquisa, publicações e participações em congressos de maneira que reflita verdadeiramente meu percurso acadêmico e minhas contribuições para a área de conhecimento",
+    description:
+      "O workshop focou em estratégias eficazes para estruturar e destacar informações no Currículo Lattes, uma ferramenta indispensável para acadêmicos, cientistas e estudantes no Brasil. Aprendi a organizar minhas qualificações, projetos de pesquisa, publicações e participações em congressos de maneira que reflita verdadeiramente meu percurso acadêmico e minhas contribuições para a área de conhecimento",
   },
 ];
 
@@ -64,7 +69,13 @@ export default function Certifications() {
       try {
         const response = await fetch("/api/list_certificates_google");
         const images = await response.json();
-        setCertificationsCloud(images.map(img => ({ title: img.split("/").pop(), image: img, type: "Google Cloud" })));
+        setCertificationsCloud(
+          images.map((img) => ({
+            title: img.split("/").pop(),
+            image: img,
+            type: "Google Cloud",
+          }))
+        );
       } catch (error) {
         console.error("Erro ao buscar certificados:", error);
       }
@@ -72,17 +83,16 @@ export default function Certifications() {
 
     fetchImages();
   }, []);
-  useEffect(() => {
-    setShowHeader(true);
-  }, []);
 
   return (
-  
     <div className="relative min-h-screen bg-white flex flex-col items-center  px-4 ">
-        {/* <Header showHeader={showHeader}/> */}
+      {/* <Header showHeader={showHeader}/> */}
       <div className="absolute top-0 left-0 w-full h-[900px] bg-white z-10">
         <svg className="absolute  bottom-0 w-full" viewBox="0 0 1440 320">
-          <path fill="#2563EB" d="M0,224L60,208C120,192,240,160,360,154.7C480,149,600,171,720,186.7C840,203,960,213,1080,208C1200,203,1320,181,1380,170.7L1440,160V320H0Z"></path>
+          <path
+            fill="#2563EB"
+            d="M0,224L60,208C120,192,240,160,360,154.7C480,149,600,171,720,186.7C840,203,960,213,1080,208C1200,203,1320,181,1380,170.7L1440,160V320H0Z"
+          ></path>
         </svg>
       </div>
       <div className="absolute left-0 top-0 flex justify-center items-center z-10">
@@ -163,7 +173,7 @@ export default function Certifications() {
       {/* Exibir certificados gerais agrupados por tipo */}
       <div
         className="w-full max-w-4xl z-10"
-      // style={{ border: "1px solid red" }}
+        // style={{ border: "1px solid red" }}
       >
         {Object.entries(groupedCertifications).map(([type, certs]) => (
           <div key={type} className="mb-10">
@@ -186,7 +196,6 @@ export default function Certifications() {
               ))}
             </div>
           </div>
-
         ))}
       </div>
       <div className="absolute   flex justify-center items-center z-0">
@@ -200,15 +209,34 @@ export default function Certifications() {
         </svg>
       </div>
       {/* Exibir certificados do Google Cloud agrupados por tipo */}
-      <div className="w-full max-w-4xl z-10eventoLattes">
-        <h2 className="text-2xl font-bold text-gray-800 capitalize mb-4">
+      <div className="w-full max-w-4xl z-10 eventoLattes">
+        <h2 className="text-2xl font-bold text-gray-800 capitalize mb-4 text-center">
           Certificados Google Cloud
         </h2>
+        <p className="text-lg text-gray-600 text-center mb-6">
+          Atualmente, sou DIAMOND no Google Cloud, um nível que reconhece
+          minha dedicação na plataforma.
+        </p>
+        <div className="flex justify-center items-center mb-6">
+          <div className="bg-white rounded-lg shadow-lg flex flex-col items-center p-4">
+            <img
+              src="/certifications/others/profile_google.png"
+              className="h-80 w-80 object-cover rounded-full"
+              alt="Perfil Google Cloud"
+            />
+          </div>
+        </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {certificationsCloud.map((cert, index) => (
-            <div key={index} className="bg-white rounded-lg shadow-lg flex flex-col items-center">
-              <img src={cert.image} alt={cert.title} className="h-80 w-full object-cover rounded-md" />
-
+            <div
+              key={index}
+              className="bg-white rounded-lg shadow-lg flex flex-col items-center"
+            >
+              <img
+                src={cert.image}
+                alt={cert.title}
+                className="h-80 w-full object-cover rounded-md"
+              />
             </div>
           ))}
         </div>
--- src/app/pages/education/page.jsx
+++ src/app/pages/education/page.jsx
@@ -1,102 +1,152 @@
+"use client";
+import { useRouter } from "next/navigation"; // Para navegação no Next.js
+
 export default function Education() {
-    return (
-      <div className="bg-gray-50 py-24 sm:py-32">
-        <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
-          <h2 className="text-center text-base/7 font-semibold text-indigo-600">high school</h2>
-          <p className="mx-auto mt-2 max-w-lg text-center text-4xl font-semibold tracking-tight text-balance text-gray-950 sm:text-5xl">
-            Minha jornada de aprendizado
-          </p>
-          <div className="mt-10 grid gap-4 sm:mt-16 lg:grid-cols-3 lg:grid-rows-2">
-            <div className="relative lg:row-span-2">
-              <div className="absolute inset-px rounded-lg bg-white lg:rounded-l-[2rem]"></div>
-              <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] lg:rounded-l-[calc(2rem+1px)]">
-                <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
-                  <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
-                   meu primeiro contato com a programação foi no ensino médio
-                  </p>
-                  <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
-                    Aprendi a programar em C e C++, JAVA, Xamarin, CSharp e a criar sites com HTML e CSS
-                  </p>
-                </div>
-                <div className="@container relative min-h-[30rem] w-full grow max-lg:mx-auto max-lg:max-w-sm">
-                  <div className="absolute inset-x-10 top-10 bottom-0 overflow-hidden rounded-t-[12cqw] border-x-[1cqw] border-t-[1cqw] border-gray-700 bg-gray-900 shadow-2xl">
-                    <img
-                      className="size-full object-cover object-top"
-                        src="/img/escola.jpg"
-                      alt=""
-                    />
-                  </div>
-                </div>
+  const router = useRouter();
+  return (
+    <div className="bg-gray-50 py-24 sm:py-32">
+    
+      <div className="absolute top-10 left-10 p-4 z-10">
+        <button
+          onClick={() => router.back()}
+          className="text-gray-700 hover:text-gray-900"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={2}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15 19l-7-7 7-7"
+            />
+          </svg>
+        </button>
+      </div>
+      <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
+        <h2 className="text-center text-base/7 font-semibold text-indigo-600">
+          high school
+        </h2>
+        <p className="mx-auto mt-2 max-w-lg text-center text-4xl font-semibold tracking-tight text-balance text-gray-950 sm:text-5xl">
+          Minha jornada de aprendizado
+        </p>
+        <div className="flex justify-center items-center relative z-10">
+        <div className="w-2/2 flex items-center justify-center">
+          <img
+            src="/avatar/sou1.webp"
+            alt="Your Name"
+            className="max-w-full rounded-full"
+          />
+        </div>
+      </div>
+        <div className="mt-10 grid gap-4 sm:mt-16 lg:grid-cols-3 lg:grid-rows-2">
+          <div className="relative lg:row-span-2">
+            <div className="absolute inset-px rounded-lg bg-white lg:rounded-l-[2rem]"></div>
+            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] lg:rounded-l-[calc(2rem+1px)]">
+              <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
+                <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
+                  ETEC - Doutor Júlio Cardoso
+                </p>
+                <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                  Minha meta era passar no vestibular da ETEC, cursar o ensino
+                  médio com qualidade e me formar já com um curso técnico
+                  integrado.
+                </p>
               </div>
-              <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5 lg:rounded-l-[2rem]"></div>
-            </div>
-            <div className="relative max-lg:row-start-1">
-              <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-t-[2rem]"></div>
-              <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
-                <div className="px-8 pt-8 sm:px-10 sm:pt-10">
-                  <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">Performance</p>
-                  <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit maiores impedit.
-                  </p>
-                </div>
-                <div className="flex flex-1 items-center justify-center px-8 max-lg:pt-10 max-lg:pb-12 sm:px-10 lg:pb-2">
+              <div className="@container relative min-h-[30rem] w-full grow max-lg:mx-auto max-lg:max-w-sm">
+                <div className="absolute inset-x-10 top-10 bottom-0 overflow-hidden rounded-t-[12cqw] border-x-[1cqw] border-t-[1cqw] border-gray-700 bg-gray-900 shadow-2xl">
                   <img
-                    className="w-full max-lg:max-w-xs"
-                    src="https://tailwindui.com/plus-assets/img/component-images/bento-03-performance.png"
+                    className="size-full object-cover object-top"
+                    src="/img/escola.jpg"
                     alt=""
                   />
                 </div>
               </div>
-              <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5 max-lg:rounded-t-[2rem]"></div>
             </div>
-            <div className="relative max-lg:row-start-3 lg:col-start-2 lg:row-start-2">
-              <div className="absolute inset-px rounded-lg bg-white"></div>
-              <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)]">
-                <div className="px-8 pt-8 sm:px-10 sm:pt-10">
-                  <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">Security</p>
-                  <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
-                    Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi.
-                  </p>
-                </div>
-                <div className="@container flex flex-1 items-center max-lg:py-6 lg:pb-2">
-                  <img
-                    className="h-[min(152px,40cqw)] object-cover"
-                    src="https://tailwindui.com/plus-assets/img/component-images/bento-03-security.png"
-                    alt=""
-                  />
-                </div>
+            <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5 lg:rounded-l-[2rem]"></div>
+          </div>
+          <div className="relative max-lg:row-start-1">
+            <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-t-[2rem]"></div>
+            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
+              <div className="px-8 pt-8 sm:px-10 sm:pt-10">
+                <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
+                  Ensino
+                </p>
+                <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center pb-5">
+                  Lá, pude aprender as disciplinas fundamentais do ensino médio,
+                  como Ciências Humanas, Ciências Exatas e Literatura, que
+                  contribuíram para a minha formação intelectual e cultural.
+                  Além disso, desenvolvi habilidades em Linguagens, aprimorando
+                  minha capacidade de comunicação, interpretação de textos e
+                  expressão escrita. Esse aprendizado não apenas fortaleceu
+                  minha base acadêmica, mas também ampliou minha visão de mundo,
+                  estimulando o pensamento crítico e a análise aprofundada dos
+                  diversos temas abordados ao longo do curso.
+                </p>
               </div>
-              <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5"></div>
             </div>
-            <div className="relative lg:row-span-2">
-              <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
-              <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
-                <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
-                  <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
-                    Powerful APIs
-                  </p>
-                  <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
-                    Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget sem sodales gravida.
-                  </p>
-                </div>
-                <div className="relative min-h-[30rem] w-full grow">
-                  <div className="absolute top-10 right-0 bottom-0 left-10 overflow-hidden rounded-tl-xl bg-gray-900 shadow-2xl">
-                    <div className="flex bg-gray-800/40 ring-1 ring-white/5">
-                      <div className="-mb-px flex text-sm/6 font-medium text-gray-400">
-                        <div className="border-r border-b border-r-white/10 border-b-white/20 bg-white/5 px-4 py-2 text-white">
-                          NotificationSetting.jsx
-                        </div>
-                        <div className="border-r border-gray-600/10 px-4 py-2">App.jsx</div>
+            <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5 max-lg:rounded-t-[2rem]"></div>
+          </div>
+          <div className="relative max-lg:row-start-3 lg:col-start-2 lg:row-start-2">
+            <div className="absolute inset-px rounded-lg bg-white"></div>
+            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)]">
+              <div className="px-8 pt-8 sm:px-10 sm:pt-10">
+                <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
+                  Meu primeiro contato com a programação foi no ensino médio{" "}
+                </p>
+                <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                  Aprendi a programar em C e C++, JAVA, Xamarin, CSharp e a
+                  criar sites com HTML e CSS
+                </p>
+              </div>
+              <div className="@container flex flex-1 items-center max-lg:py-6 lg:pb-2">
+                <img
+                  className="h-[min(152px,40cqw)] object-cover"
+                  src="https://tailwindui.com/plus-assets/img/component-images/bento-03-security.png"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5"></div>
+          </div>
+          <div className="relative lg:row-span-2">
+            <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
+            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
+              <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
+                <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
+                  Powerful APIs
+                </p>
+                <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                  Sit quis amet rutrum tellus ullamcorper ultricies libero dolor
+                  eget sem sodales gravida.
+                </p>
+              </div>
+              <div className="relative min-h-[30rem] w-full grow">
+                <div className="absolute top-10 right-0 bottom-0 left-10 overflow-hidden rounded-tl-xl bg-gray-900 shadow-2xl">
+                  <div className="flex bg-gray-800/40 ring-1 ring-white/5">
+                    <div className="-mb-px flex text-sm/6 font-medium text-gray-400">
+                      <div className="border-r border-b border-r-white/10 border-b-white/20 bg-white/5 px-4 py-2 text-white">
+                        NotificationSetting.jsx
+                      </div>
+                      <div className="border-r border-gray-600/10 px-4 py-2">
+                        App.jsx
                       </div>
                     </div>
-                    <div className="px-6 pt-6 pb-14">{/* Your code example */}</div>
+                  </div>
+                  <div className="px-6 pt-6 pb-14">
+                    {/* Your code example */}
                   </div>
                 </div>
               </div>
-              <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
             </div>
+            <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  );
+}
