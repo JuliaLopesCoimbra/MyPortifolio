c728c2b0242d78ba01c45cd0651476b33a87882c
unknown <juliacristinalopes2607@gmail.com>
2025-03-11 17:55:49 -0300

    a

--- src/app/pages/education/page.jsx
+++ src/app/pages/education/page.jsx
@@ -5,7 +5,7 @@ export default function Education() {
   const router = useRouter();
   return (
     <div className="bg-gray-100  sm:py-8">
-      <div className="absolute top-10 left-10 p-4 z-10">
+      <div className="absolute top-10 left-10 p-4 z-10" >
         <button
           onClick={() => router.back()}
           className="text-gray-700 hover:text-gray-900"
@@ -26,7 +26,7 @@ export default function Education() {
           </svg>
         </button>
       </div>
-      <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
+      <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8" >
         <h2 className="text-center text-base/7 font-semibold text-indigo-600">
           my knowledge
         </h2>
@@ -44,20 +44,20 @@ export default function Education() {
         </div>
 
         <div
-          className="mt-10 grid gap-4 sm:mt-16 lg:grid-cols-3 lg:grid-rows-2 relative bg-cover bg-center pt-10  pl-10"
-          style={{ backgroundImage: "url('/img/escola.jpg')" }}
+          className="mt-10 grid  sm:mt-16 lg:grid-cols-2 lg:grid-rows-1 relative bg-cover bg-center "
+          style={{ backgroundImage: "url('/img/escola.jpg')",  }} 
         >
-          <div className="absolute inset-0 bg-gray-900/50 rounded-2xl"></div>
+          <div className="absolute inset-0  rounded-2xl"></div>
 
-          <div className="relative max-lg:row-start-1">
-            <div className="absolute inset-px rounded-lg bg-gray-400 bg-opacity-60 max-lg:rounded-t-[2rem]"></div>
-            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
+          <div className="relative max-lg:row-start-1" >
+            <div className="absolute inset-px rounded-lg bg-gray-900 bg-opacity-60 max-lg:rounded-t-[2rem]"></div>
+            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]" >
               {/* Bloco da ETEC - Doutor Júlio Cardoso */}
-              <div className="px-8 py-6">
-                <h2 className="text-2xl font-semibold text-blue-700">
+              <div className="px-8 py-6" >
+                <h2 className="text-2xl font-semibold text-white-700">
                   ETEC - Doutor Júlio Cardoso
                 </h2>
-                <p className="mt-2 text-gray-600 leading-relaxed">
+                <p className="mt-2 text-white leading-relaxed">
                   Minha meta era passar no vestibular da ETEC, cursar o ensino
                   médio com qualidade e me formar já com um curso técnico
                   integrado.
@@ -65,8 +65,8 @@ export default function Education() {
               </div>
 
               {/* Bloco do Ensino */}
-              <div className="px-8 pt-5 sm:px-10 sm:pt-5">
-                <p className="mt-2 text-lg font-medium tracking-tight text-white max-lg:text-center">
+              <div className="px-8 pt-5 sm:px-10 sm:pt-5" >
+                <p className="mt-2 text-2xl font-medium tracking-tight text-white max-lg:text-center">
                   Ensino
                 </p>
                 <p className="mt-2 max-w-lg text-sm/6 text-white text-gray-200 max-lg:text-center pb-5">
@@ -85,25 +85,20 @@ export default function Education() {
             <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5 max-lg:rounded-t-[2rem]"></div>
           </div>
 
-          <div className="relative max-lg:row-start-3 lg:col-start-2 lg:row-start-1">
-            <div className="absolute inset-px rounded-lg bg-gray-400 bg-opacity-60"></div>
-            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)]">
-              <div className="px-8 pt-5 sm:px-10 sm:pt-5 text-white">
-                <p className="mt-2 text-lg font-medium tracking-tight text-white max-lg:text-center">
+  
+
+          <div className="relative lg:row-span-1" >
+            <div className="absolute inset-px rounded-lg bg-gray-900 bg-opacity-60 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
+            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]"
+            >
+              <div className="px-8 pt-5 pb-3 sm:px-10 sm:pt-5 sm:pb-0 text-white">
+              <p className="mt-2 text-2xl font-medium tracking-tight text-white max-lg:text-center">
                   Meu primeiro contato com a programação foi no ensino médio{" "}
                 </p>
                 <p className="mt-2 max-w-lg text-sm/6 text-gray-200 max-lg:text-center">
                   Aprendi a programar em C e C++, JAVA, Xamarin, CSharp e a
                   criar sites com HTML e CSS
                 </p>
-              </div>
-            </div>
-          </div>
-
-          <div className="relative lg:row-span-2">
-            <div className="absolute inset-px rounded-lg bg-gray-400 bg-opacity-60 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
-            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
-              <div className="px-8 pt-5 pb-3 sm:px-10 sm:pt-5 sm:pb-0 text-white">
                 <p className="mt-2 text-lg font-medium tracking-tight text-white max-lg:text-center">
                   Aprendizado com o curso
                 </p>
@@ -137,6 +132,19 @@ export default function Education() {
             <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
           </div>
         </div>
+
+        
+        <div
+        className="relative bg-cover bg-center"
+        style={{ backgroundImage: "url('/img/facef.jpg')", border:"solid red 1px" }}
+      >
+        <div className="absolute inset-0 bg-white/30 blur-lg"></div>
+        <div className="relative">
+          oi
+        </div>
+      </div>
+        
+
       </div>
     </div>
   );
